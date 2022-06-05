0d9b6d5487591b4fe1312ebd19bcd406dea96c15
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2022-06-05 08:27:58 +0300

    Fix Jakarta remote annotation.

--- src/main/java/js/tools/apt/GenerateRmiScript.java
+++ src/main/java/js/tools/apt/GenerateRmiScript.java
@@ -26,8 +26,7 @@ import js.tools.script.gen.JsMethod;
 
 /**
  * Annotation processor for HTTP-RMI stub script generation. Process all remote methods from classes annotated with
- * "js.annotation.Remote" and generate script classes for HTTP-RMI stub, usable by applications based on j(s)-lib Client
- * library.
+ * "jakarta.ejb.Remote" - deprecated "javax.ejb.Remote" is also supported, and generate script classes for HTTP-RMI stub.
  * 
  * <pre>
  * In order to use this annotation processor from Eclipse IDE one may need to:
@@ -45,150 +44,137 @@ import js.tools.script.gen.JsMethod;
  * @author Iulian Rotaru
  * @since 1.0
  */
-@SupportedAnnotationTypes(
-{
-  "javax.ejb.Remote", "jakarta.ejb.Remote"
-})
-public class GenerateRmiScript extends AbstractProcessor
-{
-  /**
-   * Annotation processor files factory.
-   */
-  private Filer filer;
+@SupportedAnnotationTypes({ "jakarta.ejb.Remote", "javax.ejb.Remote" })
+public class GenerateRmiScript extends AbstractProcessor {
+	/**
+	 * Annotation processor files factory.
+	 */
+	private Filer filer;
 
-  @Override
-  public void init(ProcessingEnvironment env)
-  {
-    this.filer = env.getFiler();
-  }
+	@Override
+	public void init(ProcessingEnvironment env) {
+		this.filer = env.getFiler();
+	}
 
-  @Override
-  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env)
-  {
-    for(Element element : env.getRootElements()) {
-      if(element.getKind() != ElementKind.INTERFACE && element.getKind() != ElementKind.CLASS) {
-        continue;
-      }
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
+		for (Element element : env.getRootElements()) {
+			if (element.getKind() != ElementKind.INTERFACE && element.getKind() != ElementKind.CLASS) {
+				continue;
+			}
 
-      TypeElement typeElement = (TypeElement)element;
-      boolean remoteType = isRemote(typeElement);
+			TypeElement typeElement = (TypeElement) element;
+			boolean remoteType = isRemote(typeElement);
 
-      JsClass jsClass = Builder.createJsClass();
-      jsClass.setQualifiedClassName(typeElement.getQualifiedName().toString());
+			JsClass jsClass = Builder.createJsClass();
+			jsClass.setQualifiedClassName(typeElement.getQualifiedName().toString());
 
-      for(Element innerElement : element.getEnclosedElements()) {
-        if(innerElement.getKind() != ElementKind.METHOD) {
-          continue;
-        }
-        ExecutableElement methodElement = (ExecutableElement)innerElement;
-        if(isNotPublic(methodElement)) {
-          continue;
-        }
-        if(isLocal(methodElement)) {
-          continue;
-        }
-        if(!remoteType && !isRemote(methodElement)) {
-          continue;
-        }
+			for (Element innerElement : element.getEnclosedElements()) {
+				if (innerElement.getKind() != ElementKind.METHOD) {
+					continue;
+				}
+				ExecutableElement methodElement = (ExecutableElement) innerElement;
+				if (isNotPublic(methodElement)) {
+					continue;
+				}
+				if (isLocal(methodElement)) {
+					continue;
+				}
+				if (!remoteType && !isRemote(methodElement)) {
+					continue;
+				}
 
-        JsMethod jsMethod = jsClass.createMethod();
-        jsMethod.setMethodName(methodElement.getSimpleName().toString());
-        jsMethod.setReturnType(methodElement.getReturnType().toString());
+				JsMethod jsMethod = jsClass.createMethod();
+				jsMethod.setMethodName(methodElement.getSimpleName().toString());
+				jsMethod.setReturnType(methodElement.getReturnType().toString());
 
-        for(Element parameterElement : methodElement.getParameters()) {
-          String type = parameterElement.asType().toString();
-          String name = parameterElement.getSimpleName().toString();
-          jsMethod.addParameter(type, name);
-        }
-        for(TypeMirror thrownElement : methodElement.getThrownTypes()) {
-          jsMethod.addExceptionType(thrownElement.toString());
-        }
-        jsClass.addMethod(jsMethod);
-      }
-      if(!jsClass.hasMethods()) {
-        continue;
-      }
+				for (Element parameterElement : methodElement.getParameters()) {
+					String type = parameterElement.asType().toString();
+					String name = parameterElement.getSimpleName().toString();
+					jsMethod.addParameter(type, name);
+				}
+				for (TypeMirror thrownElement : methodElement.getThrownTypes()) {
+					jsMethod.addExceptionType(thrownElement.toString());
+				}
+				jsClass.addMethod(jsMethod);
+			}
+			if (!jsClass.hasMethods()) {
+				continue;
+			}
 
-      String fileName = jsClass.getClassName() + ".js";
-      Writer writer = null;
-      try {
-        FileObject file = this.filer.createResource(StandardLocation.SOURCE_OUTPUT, jsClass.getPackageName(), fileName, element);
-        writer = file.openWriter();
-        jsClass.serialize(writer);
-      }
-      catch(IOException e) {
-        e.printStackTrace();
-      }
-      finally {
-        close(writer);
-      }
-    }
-    return true;
-  }
+			String fileName = jsClass.getClassName() + ".js";
+			Writer writer = null;
+			try {
+				FileObject file = this.filer.createResource(StandardLocation.SOURCE_OUTPUT, jsClass.getPackageName(), fileName, element);
+				writer = file.openWriter();
+				jsClass.serialize(writer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				close(writer);
+			}
+		}
+		return true;
+	}
 
-  /**
-   * Helper method to close a closeable instance, possible null. This method also takes care to print exception stack if
-   * close operation fails.
-   * 
-   * @param closeable closeable instance, possible null.
-   */
-  private static void close(Closeable closeable)
-  {
-    if(closeable != null) {
-      try {
-        closeable.close();
-      }
-      catch(IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
+	/**
+	 * Helper method to close a closeable instance, possible null. This method also takes care to print exception stack if close
+	 * operation fails.
+	 * 
+	 * @param closeable closeable instance, possible null.
+	 */
+	private static void close(Closeable closeable) {
+		if (closeable != null) {
+			try {
+				closeable.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-  /**
-   * Helper predicate to test if method modifier is private or protected.
-   * 
-   * @param methodElement method element.
-   * @return true if method is private or protected.
-   */
-  private static boolean isNotPublic(ExecutableElement methodElement)
-  {
-    return methodElement.getModifiers().contains(Modifier.PRIVATE) || methodElement.getModifiers().contains(Modifier.PROTECTED);
-  }
+	/**
+	 * Helper predicate to test if method modifier is private or protected.
+	 * 
+	 * @param methodElement method element.
+	 * @return true if method is private or protected.
+	 */
+	private static boolean isNotPublic(ExecutableElement methodElement) {
+		return methodElement.getModifiers().contains(Modifier.PRIVATE) || methodElement.getModifiers().contains(Modifier.PROTECTED);
+	}
 
-  /**
-   * Helper method to test if class or method is annotated as {@literal @}Remote.
-   * 
-   * @param element class or method to test.
-   * @return true if element is annotated with {@literal @}Remote.
-   */
-  private static boolean isRemote(Element element)
-  {
-    for(AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
-      String annotationType = annotationMirror.getAnnotationType().asElement().getSimpleName().toString();
-      if(annotationType.toString().equals("Service")) {
-        return true;
-      }
-      if(annotationType.toString().equals("Remote")) {
-        return true;
-      }
-    }
-    return false;
-  }
+	/**
+	 * Helper method to test if class or method is annotated as {@literal @}Remote.
+	 * 
+	 * @param element class or method to test.
+	 * @return true if element is annotated with {@literal @}Remote.
+	 */
+	private static boolean isRemote(Element element) {
+		for (AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
+			String annotationType = annotationMirror.getAnnotationType().asElement().getSimpleName().toString();
+			if (annotationType.toString().equals("Service")) {
+				return true;
+			}
+			if (annotationType.toString().equals("Remote")) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  /**
-   * Helper method to test if class or method is annotated as {@literal @}Local.
-   * 
-   * @param element class or method to test.
-   * @return true if element is annotated with {@literal @}Local.
-   */
-  private static boolean isLocal(Element element)
-  {
-    for(AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
-      String annotationType = annotationMirror.getAnnotationType().asElement().getSimpleName().toString();
-      if(annotationType.toString().equals("Local")) {
-        return true;
-      }
-    }
-    return false;
-  }
+	/**
+	 * Helper method to test if class or method is annotated as {@literal @}Local.
+	 * 
+	 * @param element class or method to test.
+	 * @return true if element is annotated with {@literal @}Local.
+	 */
+	private static boolean isLocal(Element element) {
+		for (AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
+			String annotationType = annotationMirror.getAnnotationType().asElement().getSimpleName().toString();
+			if (annotationType.toString().equals("Local")) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
